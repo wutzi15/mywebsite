4137c93b7e83dbe54f2b3d160efc0a6941a349a4
Benedikt Bergenthal <benedikt.bergenthal@imflow.me>
2020-12-31 15:24:23 +0100

    added missing files to git
    Signed-off-by: Benedikt Bergenthal <benedikt.bergenthal@imflow.me>

---
+++ frontend/src/views/Footer.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="hero-foot hero is-dark">
+    <nav class="tabs is-boxed is-fullwidth">
+      <div class="container">
+        <ul>
+          <li><a href="">Impressum</a></li>
+          <li><a href="">Datenschutz</a></li>
+        </ul>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+name: 'Footer',
+}
+</script>
+
+<style>
+
+</style>
