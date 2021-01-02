8f206abec00e3812c7b109197dde31353f99c414
Benedikt Bergenthal <benedikt@kdrennert.de>
2021-01-02 11:19:33 +0100

    added missing vue files
    Signed-off-by: Benedikt Bergenthal <benedikt@kdrennert.de>

---
+++ frontend/src/components/BlogListItem.vue
@@ -0,0 +1,35 @@
+<template>
+<div class="">
+  <h2>{{ title }}</h2>
+  <h3>{{ myDate }}</h3>
+</div>
+</template>
+
+<script>
+export default {
+  name: "BlogListItem",
+  props: {
+    title: {
+      type: String,
+      default: ""
+    },
+    datecreated: {
+      type: String,
+      default: ""
+    }
+  },
+  computed: {
+    myDate: function() {
+      const d = new Date(this.datecreated);
+      return d.toLocaleString(navigator.language);
+    }
+
+  }
+
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ frontend/src/views/SingleEntry.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="">
+    <h1>{{ entry.title }}</h1>
+    <div v-html="myhtml"></div>
+  </div>
+</template>
+
+<script>
+import showdown from 'showdown';
+
+export default {
+  name: "SingleEntry",
+  props: {
+    entry: {
+      type: Object
+    },
+
+  },
+  data: function() {
+    return {
+      myhtml : ""
+    }
+  },
+  mounted() {
+    this.convertText();
+  },
+  methods: {
+    convertText: function() {
+      console.log(`Content: ${this.entry.content}`)
+      const converter = new showdown.Converter();
+      this.myhtml = converter.makeHtml(this.entry.content);
+
+    }
+  }
+
+}
+</script>
+
+<style>
+
+</style>
