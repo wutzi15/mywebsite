bf3b79f534fd2230114f9c679d9fa1de525dd766
Benedikt Bergenthal <benedikt@kdrennert.de>
2021-01-02 15:38:49 +0100

    Syntachighlighting with highlightjs working
    Signed-off-by: Benedikt Bergenthal <benedikt@kdrennert.de>

--- frontend/src/views/SingleEntry.vue
+++ frontend/src/views/SingleEntry.vue
@@ -2,9 +2,8 @@
   <div class="columns">
     <div class="column"></div>
     <div class="column is-half">
-    <h1>{{ entry.title }}</h1>
-    <div v-html="myhtml"></div>
-
+      <h1>{{ entry.title }}</h1>
+      <div v-html="myhtml"></div>
     </div>
     <div class="column"></div>
   </div>
@@ -13,6 +12,8 @@
 <script>
 import showdown from 'showdown';
 import axios from "axios";
+import hljs from "highlightjs";
+
 
 export default {
   name: "SingleEntry",
@@ -37,8 +38,22 @@ export default {
         this.entry = results.data;
         const converter = new showdown.Converter();
         const tmp = converter.makeHtml(this.entry.content);
-        this.myhtml = tmp.replace('<img src="/uploads', '<img src="https://api.benedikt-bergenthal.de/uploads');
-      } catch (error) {
+
+        const tmpIMG = tmp.replace('<img src="/uploads', '<img src="https://api.benedikt-bergenthal.de/uploads');
+        const dom = document.createElement('div');
+        dom.innerHTML = tmpIMG;
+        const codes = dom.querySelectorAll('code');
+        for(var i = 0; i < codes.length; ++i) {
+          const code = codes[i];
+          console.log(`InnerHTML: ${code.innerHTML}`)
+          const highlighted = hljs.highlightAuto(code.innerHTML).value;
+          code.innerHTML = highlighted;
+        }
+
+
+        this.myhtml = dom.innerHTML;
+
+      }catch(error){
         console.error(error);
       }
       // console.log(`Content: ${this.entry.content}`)
@@ -50,7 +65,7 @@ export default {
 
 <style lang="scss" >
 code {
-  font-family: 'Fira Code', monospace;
+  font-family: "Fira Code", monospace;
   color: #333;
 }
 
@@ -65,8 +80,7 @@ h1 {
   font-size: 3rem;
 }
 
-h2{
+h2 {
   font-size: 2rem;
 }
-
 </style>
